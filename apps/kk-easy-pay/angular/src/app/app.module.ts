a24e4f4955e2c8c6a273443257f6fc2ab038ba85
elephantent <elephantent@outlook.com>
2023-06-09 14:59:41 +0200

    list, routing, etc

--- apps/kk-easy-pay/angular/src/app/app.module.ts
+++ apps/kk-easy-pay/angular/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { HttpClientModule } from '@angular/common/http';
+import { LeistungsPaketDisplayComponent } from './leistungs-paket-display/leistungs-paket-display.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LeistungsPaketDisplayComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
---
+++ apps/kk-easy-pay/angular/src/app/leistungs-paket-display/leistungs-paket-display.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+import { LeistungsPaket } from '@pct/kk-easy-pay-common'
+
+
+@Component({
+  selector: 'pct-leistungs-paket-display',
+  templateUrl: './leistungs-paket-display.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+})
+export class LeistungsPaketDisplayComponent {
+  @Input() paket: LeistungsPaket = {
+    nr: 1,
+    description: 'Test Descr',
+    price: 123,
+  };
+
+  get priceEuro() {
+    return (this.paket.price /100).toFixed(2) + '€';
+  }
+}
